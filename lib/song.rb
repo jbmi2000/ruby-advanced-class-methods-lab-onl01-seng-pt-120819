c29dc832547af2e30ff198ba402f3e19b4a0ec71
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-16 03:07:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,5 +43,12 @@ class Song
     @@all.sort_by{|s| s.name}
   end
 
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
 
 end
